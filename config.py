73c35629792c11561dc42242eaf14c52830cc3ef
sujalchavdaa <sujalchavda500@gmail.com>
2025-06-05 14:39:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id = 28660719
-api_hash = "34ade52147ebcaf4bc0b88aa3dfc7e1a"
-bot_token = "7001245667:AAGm3VkNigvYgO5nrB9Civflz9lop0B91d8"
-auth_users = "6990313439"
+api_id = 26330942
+api_hash = "5de9fd033aa828dfd3bf0c28adeee660"
+bot_token = "7999358042:AAGXpAPxJkftNoUVKtzq4aQsmbs32wBEmf0"
+auth_users = "6883471516"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "6990313439"
+osowner_users = "6883471516"
 owner_users = [int(num) for num in osowner_users.split(",")]
